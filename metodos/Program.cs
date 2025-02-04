42dcc182c7ccbdb71ca85a4b34eecef9d4dee3c5
Vinicius <vinicius.paiva2@outlook.com>
2025-02-04 11:23:51 -0300

    Temporariamente movido metodos

---
+++ metodos/Program.cs
@@ -0,0 +1,28 @@
+﻿using Menu;
+
+static int Teste(int arg)
+{
+    while(!int.TryParse(Console.ReadLine(), out arg))
+    {
+        Console.Write("Error, tente novamente!");
+    }
+    return arg;
+}
+
+static void Main(string[] args)
+{
+    string opcao = "";
+    int n1 = 0;
+    int n2 = 0;
+
+    opcao = Navegacao.ImprimirMenu();
+
+    switch (opcao)
+    {
+        case "1":
+            Console.Write("Informe o primeiro número: ");
+
+            Teste(n1);
+            break;
+    }
+}
